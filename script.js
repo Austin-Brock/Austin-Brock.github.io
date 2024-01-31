c6f4a3d6ac01774954658535efd11ff82a7e4e36
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-01-31 17:17:05 -0500

    uploaded files

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+document.querySelector( "#retrobg-sun" ).onclick = () => {
+  document.querySelector( "#retrobg" ).classList.toggle( "retrobg-shutdown" );
+};
