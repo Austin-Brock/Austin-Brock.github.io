1ed1ee42d5e34375d87ccd50595922316f13fb0a
Austin-Brock <135121147+Austin-Brock@users.noreply.github.com>
2024-02-01 13:50:59 -0500

    uploading original js file

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.querySelector( "#retrobg-sun" ).onclick = () => {
-  document.querySelector( "#retrobg" ).classList.toggle( "retrobg-shutdown" );
-};
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
