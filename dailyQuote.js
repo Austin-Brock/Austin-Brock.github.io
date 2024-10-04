a0612c80ed931c409d0bbfb80371126652f7b859
Austin-Brock <austin24brock@gmail.com>
2024-10-04 12:14:32 -0400

    added a quote to the js file

--- dailyQuote.js
+++ dailyQuote.js
@@ -6,7 +6,8 @@ const quotes = [
     { text: "Success is not the key to happiness. Happiness is the key to success.", author: "Albert Schweitzer" },
     { text: "What lies behind us and what lies before us are tiny matters compared to what lies within us.", author: "Ralph Waldo Emerson" },
     { text: "Life is 10% what happens to us and 90% how we react to it.", author: "Charles R. Swindoll" },
-    { text: "The best time to plant a tree was 20 years ago. The second best time is now.", author: "Chinese Proverb" }
+    { text: "The best time to plant a tree was 20 years ago. The second best time is now.", author: "Chinese Proverb" },
+    { text: "Luck is when preparation meets opportunity.", author: "Seneca"}
 ];
 
 // Function to get today's quote based on the current date
